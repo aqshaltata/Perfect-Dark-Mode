ea6075e7e61fca9f5032e04ec033d268df8e3cde
Aqshal Tata <69721022+aqshaltata@users.noreply.github.com>
2020-08-25 08:05:35 +0800

    Update perfectdarkmode.js

--- perfectdarkmode.js
+++ perfectdarkmode.js
@@ -3,25 +3,28 @@ const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"
 const currentTheme = localStorage.getItem('theme');
 var date = new Date();
 
-// Jika jam di perangkat pengguna lebih dari jam 6 sore dan kurang dari jam 6 pagi (MALAM)
-if(date.getHours() > 18 || date.getHours() < 6){
-	toggleSwitch.checked = true;
-	document.documentElement.setAttribute('data-theme', 'dark');
-	localStorage.setItem('theme', 'dark');
-}
-// Sebaliknya
-else{
-	toggleSwitch.checked = false;
-	document.documentElement.setAttribute('data-theme', 'light');
-	localStorage.setItem('theme', 'light');
-}
 // Jika Pengguna pernah mengunjungi website / blog anda
 if (currentTheme) {
-  document.documentElement.setAttribute('data-theme', currentTheme);
-  
-    if (currentTheme === 'dark') {
-    toggleSwitch.checked = true;
-    }
+	document.documentElement.setAttribute('data-theme', currentTheme);
+
+	if (currentTheme === 'dark') {
+	toggleSwitch.checked = true;
+	}
+}
+// Jika ini yang pertama
+else{
+	// Jika jam di perangkat pengguna lebih dari jam 6 sore dan kurang dari jam 6 pagi (MALAM)
+	if(date.getHours() > 18 || date.getHours() < 6){
+		toggleSwitch.checked = true;
+		document.documentElement.setAttribute('data-theme', 'dark');
+		localStorage.setItem('theme', 'dark');
+	}
+	// Sebaliknya
+	else{
+		toggleSwitch.checked = false;
+		document.documentElement.setAttribute('data-theme', 'light');
+		localStorage.setItem('theme', 'light');
+	}
 }
 function switchTheme(e) {
     if (e.target.checked) {
